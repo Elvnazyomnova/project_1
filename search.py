101d3d0762cd6fb02196a24af36b09c605b6b5d2
Elvira Nazyomnova <zalalova.e@yandex.ru>
2022-12-11 12:40:09 +0400

    Update and rename Jump_search.py to search.py

--- search.py
+++ search.py
@@ -1,3 +1,4 @@
+#jump searsch
 import math
 from random import randint
 
@@ -32,3 +33,30 @@ if JumpSearch(testarr, x) != -1:
   print ("Элемент под индексом", JumpSearch(testarr,x))
 else:
   print ("Элемент отсутствует в массиве")
+  
+#Interpolation Search
+import random
+from random import randint
+
+def InterpolationSearch(arr, value):
+    low = 0
+    high = (len(arr) - 1)
+    while low <= high and value >= arr[low] and value <= arr[high]:
+        index = low + int(((float(high - low) / (arr[high] - arr[low])) * (value - arr[low])))
+        if arr[index] == value:
+            return index
+        if arr[index] < value:
+            low = index + 1
+        else:
+            high = index - 1
+    return -1
+testarr = []
+for i in range(10):
+    testarr.append(randint(1, 100))
+    testarr.sort()
+print(testarr)
+x = int(input('Введи число:'))
+if InterpolationSearch(testarr,x) != -1:
+    print ("Элемент найден в массиве, под индексом:,", InterpolationSearch(testarr,x))
+else:
+    print ("Элемент не найден в массиве")
